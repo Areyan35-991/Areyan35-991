746bb2078efaf73e7f002f55dedd3db97980e1e5
Areyan35-991 <106174602+Areyan35-991@users.noreply.github.com>
2022-05-25 11:14:37 +0600

    Add files via upload

---
+++ basic of input a.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+
+      int a;
+
+      printf("Entire the value of a:");
+      scanf("%d",&a);
+      printf("Number:%d",a);
+
+
+
+      return 0;
+
+
+
+
+
+
+
+
+
+
+}
